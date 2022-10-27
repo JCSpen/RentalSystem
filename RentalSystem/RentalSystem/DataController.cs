6c46f6eb62df4e254de50862b0bafb57572b9ef4
Jamie Spence <103535034+JCSpen@users.noreply.github.com>
2022-10-27 22:08:35 +0100

    Add files via upload

---
+++ RentalSystem/RentalSystem/DataController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+
+public class DataController
+    {
+    static string DataSource = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source= " + Application.StartupPath + @"\MightyMotorsDB.accdb";
+    public OleDbConnection Connection = new OleDbConnection(DataSource);
+   
+    public void CreateUser(string Username,string Password, string FirstName, string LastName, string Insurance, int ID)
+    {
+        OleDbCommand Command = new OleDbCommand(DataSource);
+        Connection.Open();
+        Command.CommandText = "INSERT INTO Users([ID],Username,[Password],FirstName,LastName,InsuranceProvider) VALUES(" + ID + ",'" + Username + "','" + Password + "','" + FirstName + "','" + LastName + "','" + Insurance + "');";
+        Command.Connection = Connection;
+        Command.ExecuteNonQuery();
+        Connection.Close();
+    }
+
+    public int FindUserID(string Username, string Password)
+    {
+        OleDbCommand Command = new OleDbCommand(DataSource);
+        Connection.Open();
+        Command.CommandText = "SELECT ID FROM Users WHERE(Username = '"+ Username + "' AND [Password] = '"+Password + "');";
+        Command.Connection = Connection;
+        Command.CommandType = System.Data.CommandType.Text;
+        int result = (int)Command.ExecuteScalar();
+        Connection.Close();
+        return result;
+    }
+}
+
---
+++ RentalSystem/RentalSystem/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    public class User
+    {
+    public string Username;
+    public string Password;
+    public string FirstName;
+    public string LastName;
+    public string InsuranceProvider;
+    public bool NewUser;
+    public int ID;
+    }
+
